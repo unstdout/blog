0b1f73088f505c1b5a3940296b4479a103c5fdb4
Joshua Rubin <jrubin@zvelo.com>
2014-04-30 03:00:35 -0600

    start adding hugo content
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

--- server/server.go
+++ server/server.go
@@ -6,9 +6,18 @@ import (
 )
 
 func init() {
-	http.HandleFunc("/go", handler)
+	http.HandleFunc("/", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	const INDEX = "/index.html"
+	pos := len(r.RequestURI) - len(INDEX)
+	if pos > 0 && r.RequestURI[pos:] != INDEX {
+		// TODO(jrubin) prevent ...//index.html
+		w.Header().Set("Location", r.RequestURI+INDEX)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+	http.NotFound(w, r)
 	fmt.Fprint(w, "The opposite of standard.")
 }
